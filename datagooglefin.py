bacbc942f3fb3f7a8335f16bc1d9c7d1a61e1978
Tjebbe Hepkema <Tjebbe@Laptop-van-Tjebbe.local>
2016-09-22 21:19:11 +0200

    eerste commit
    pif

---
+++ datagooglefin.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+
+startdate = "24 09 2011"
+enddate = "24 09 2016"
+startdate = dt.datetime.strptime(startdate, '%d %m %Y').strftime('%b+%d%%2C+%Y')
+enddate = dt.datetime.strptime(enddate, '%d %m %Y').strftime('%b+%d%%2C+%Y')
+
+bedrijven = {"apple": 22144, "netflix": 672501}
+
+url={}
+
+for bedrijf,number in bedrijven.items():
+    url[bedrijf] = "http://www.google.com/finance/historical?cid=%s&startdate=%s&enddate=%s&num=30&ei=8R7kV4PHEpWRUNSzjuAC&output=csv" %(number, startdate,enddate)
+
+df_apple = pd.read_csv(url["apple"], index_col="\xef\xbb\xbfDate")
+
+
+df_apple = df_apple.iloc[::-1]
+#df_apple["Volume"].plot()
+#
+#plt.show()
